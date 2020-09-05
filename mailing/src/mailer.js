8945a73b56f22452501e4d52dfdb4a4067faf20e
Pratik Priyadarsan <priyadarshanpratik123@gmail.com>
2020-09-05 15:49:20 +0530

    Done to enable easy mail sending in various websites of mine

---
+++ mailing/src/mailer.js
@@ -0,0 +1,50 @@
+const nodemailer = require('nodemailer');
+const express = require('express')
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const {
+    response
+} = require('express');
+
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+
+
+app.route('/send-mail').post((req, res) => {
+        const service = req.query.service;
+        const fromMail = req.query.email;
+        const pwd = req.query.password;
+        const other = req.query.other;
+        const sub = req.query.sub;
+        const body = req.query.body;
+        const transporter = nodemailer.createTransport({
+            service: service,
+            auth: {
+                user: fromMail,
+                pass: pwd
+            }
+        })
+        const mailOptions = {
+            from: fromMail,
+            to: other,
+            subject: sub,
+            text: body
+        }
+        transporter.sendMail(mailOptions, (err, response) => {
+            if (err)
+                return res.send({
+                    msg: 'Some error Occured,try again'
+                })
+            res.send({
+                msg: `Mail Sent Successfully from ${fromMail} to ${other}`,
+                status :'Success',
+                report: response
+            });
+        })
+    })
+
+
+app.listen(3000, (err) => console.log(err));
